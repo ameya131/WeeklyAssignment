9ad55e8db242fb7886eb0fc2332d812b8c94a7f1
ameya131 <guptas.ameya@gmail.com>
2026-03-11 14:47:07 +0530

    Ameya added P10

--- App/src/weekly.java
+++ App/src/weekly.java
@@ -2,18 +2,27 @@ import java.util.*;
 
 public class weekly{
 
-    static void twoSum(int[] a,int target){
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<a.length;i++){
-            int c=target-a[i];
-            if(map.containsKey(c))
-                System.out.println(map.get(c)+" "+i);
-            map.put(a[i],i);
+    static LinkedHashMap<String,String> L1=
+            new LinkedHashMap<>(10,0.75f,true){
+                protected boolean removeEldestEntry(Map.Entry e){
+                    return size()>3;
+                }
+            };
+
+    static HashMap<String,String> DB=new HashMap<>();
+
+    static String get(String id){
+        if(L1.containsKey(id)) return "L1 HIT";
+        if(DB.containsKey(id)){
+            L1.put(id,DB.get(id));
+            return "DB HIT → moved to L1";
         }
+        return "NOT FOUND";
     }
 
     public static void main(String[] args){
-        int[] t={500,300,200};
-        twoSum(t,500);
+        DB.put("v1","video1");
+        System.out.println(get("v1"));
+        System.out.println(get("v1"));
     }
 }
